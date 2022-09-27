cdcbded245eeb1681177375c889abcdce603cf70
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-09-27 19:53:53 +0200

    created project

---
+++ S5T1N1/src/main/java/com/MartinFluviaPablo/S5T1N1/S5T1N1Application.java
@@ -0,0 +1,13 @@
+package com.MartinFluviaPablo.S5T1N1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5T1N1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5T1N1Application.class, args);
+	}
+
+}
---
+++ S5T1N1/src/test/java/com/MartinFluviaPablo/S5T1N1/S5T1N1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MartinFluviaPablo.S5T1N1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5T1N1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
